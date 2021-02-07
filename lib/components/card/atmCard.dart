0272f5347d1bb33723954514113e4b45dc1021ff
mg143pavankumar <pavansharma.mg143@gmail.com>
2021-02-07 21:32:45 +0530

    updated UI

--- lib/components/card/atmCard.dart
+++ lib/components/card/atmCard.dart
@@ -2,121 +2,114 @@ import 'package:basic_banking_app/constants/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-
 class UserATMCard extends StatefulWidget {
-
-  final String cardNumber, cardHolderName,cardExpiryDate;
+  final String cardNumber, cardHolderName, cardExpiryDate;
+  final gradientColor;
 
   UserATMCard({
     Key key,
     @required this.cardNumber,
     @required this.cardHolderName,
     @required this.cardExpiryDate,
-    
+    this.gradientColor,
   });
-  
+
   @override
   _UserATMCardState createState() => _UserATMCardState();
 }
 
 class _UserATMCardState extends State<UserATMCard> {
-  
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.only(right: mgDefaultPadding/2),
-         height: 199,
-         width: 344,
-         decoration: BoxDecoration(
-           gradient: mgPrimaryGradient,
-           borderRadius: BorderRadius.all(
-             Radius.circular(28),
-           )
-         ),
-         child: Stack(
-           children: [
-             Positioned(
-               left: 29,
-               top: 48,
-               child: Text(
-                 "CARD NUMBER",
-                 style: Theme.of(context).textTheme.subtitle2.copyWith(
-                   color: Colors.white,
-                   fontWeight: FontWeight.w300,
-                   letterSpacing: .5
-                 ),
-                 ),
-              ),
-             Positioned(
-               left: 29,
-               top: 65,
-               child: Text(
-                 widget.cardNumber,
-                 style: Theme.of(context).textTheme.headline6.copyWith(
-                   color: Colors.white,
-                   fontWeight: FontWeight.w700,
-                   letterSpacing: .5
-                 ),
-                 ),
-              ),
-             Positioned(
-               right: 21,
-               top: 35,
-               child:  SvgPicture.asset(cardsIcon[0], width: 40,),
-               
-              ),
-             Positioned(
-               left: 29,
-               bottom: 45,
-               child: Text(
-                 "CARD HOLDER NAME",
-                 style: Theme.of(context).textTheme.subtitle2.copyWith(
-                   color: Colors.white,
-                   fontWeight: FontWeight.w300,
-                   letterSpacing: .5
-                 ),
-                 ),
-              ), 
-             Positioned(
-               left: 29,
-               bottom: 20,
-               child: Text(
-                 widget.cardHolderName,
-                 style: Theme.of(context).textTheme.headline6.copyWith(
-                   color: Colors.white,
-                   fontSize: 20,
-                   fontWeight: FontWeight.w700,
-                   letterSpacing: .5
-                 ),
-                 ),
-              ), 
-             Positioned(
-               right: 23,
-               bottom: 45,
-               child: Text(
-                 "EXPIRY DATE",
-                 style: Theme.of(context).textTheme.subtitle2.copyWith(
-                   color: Colors.white,
-                   fontWeight: FontWeight.w300,
-                   letterSpacing: .5
-                 ),
-                 ),
-              ), 
-             Positioned(
-               right: 23,
-               bottom: 20,
-               child: Text(
-                 widget.cardExpiryDate,
-                 style: Theme.of(context).textTheme.headline6.copyWith(
-                   color: Colors.white,
-                   fontSize: 16,
-                   fontWeight: FontWeight.w700,
-                   letterSpacing: .5
-                 ),
-                 ),
-              ), 
-           ],
-         ),
+      margin: const EdgeInsets.only(right: mgDefaultPadding / 2),
+      height: 199,
+      width: 344,
+      decoration: BoxDecoration(
+          gradient: widget.gradientColor,
+          borderRadius: BorderRadius.all(
+            Radius.circular(28),
+          )),
+      child: Stack(
+        children: [
+          Positioned(
+            left: 29,
+            top: 48,
+            child: Text(
+              "CARD NUMBER",
+              style: Theme.of(context).textTheme.subtitle2.copyWith(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w300,
+                  letterSpacing: .5),
+            ),
+          ),
+          Positioned(
+            left: 29,
+            top: 65,
+            child: Text(
+              widget.cardNumber,
+              style: Theme.of(context).textTheme.headline6.copyWith(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w700,
+                  letterSpacing: .5),
+            ),
+          ),
+          Positioned(
+            right: 21,
+            top: 35,
+            child: SvgPicture.asset(
+              cardsIcon[0],
+              width: 40,
+            ),
+          ),
+          Positioned(
+            left: 29,
+            bottom: 45,
+            child: Text(
+              "CARD HOLDER NAME",
+              style: Theme.of(context).textTheme.subtitle2.copyWith(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w300,
+                  letterSpacing: .5),
+            ),
+          ),
+          Positioned(
+            left: 29,
+            bottom: 20,
+            child: Text(
+              widget.cardHolderName,
+              style: Theme.of(context).textTheme.headline6.copyWith(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w700,
+                  letterSpacing: .5),
+            ),
+          ),
+          Positioned(
+            right: 23,
+            bottom: 45,
+            child: Text(
+              "EXPIRY DATE",
+              style: Theme.of(context).textTheme.subtitle2.copyWith(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w300,
+                  letterSpacing: .5),
+            ),
+          ),
+          Positioned(
+            right: 23,
+            bottom: 20,
+            child: Text(
+              widget.cardExpiryDate,
+              style: Theme.of(context).textTheme.headline6.copyWith(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                  letterSpacing: .5),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ lib/screens/transferMoney.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TransferMoney extends StatefulWidget {
+  @override
+  _TransferMoneyState createState() => _TransferMoneyState();
+}
+
+class _TransferMoneyState extends State<TransferMoney> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
